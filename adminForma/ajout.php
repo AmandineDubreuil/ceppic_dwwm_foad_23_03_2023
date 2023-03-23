281f89f82b5a51c39dbc42c832af22cd75e3004b
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2023-03-23 12:40:08 +0100

    ajout ajout

---
+++ adminForma/ajout.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* Ajout d'une formation
+*/
+session_start();
+include '../inc/fonctions.php';
+
+(isUserLogin()) ?: redirectUrl('view/404.php');
+
+
+$titre = $description = $imageUpload = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["upload"])) :
+
+    imageUpload($imageUpload);
+
+endif;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["ajout"])) :
+
+    $titre = cleanData($_POST['titre']);
+    $imageUpload = cleanData($_POST['imageUpload']);
+    $description = cleanData($_POST['description']);
+
+        insertFormation($titre, $description, $imageUpload);
+
+  //  redirectUrl('./adminForma/');
+endif;
+
+require '../view/adminForma/ajoutView.php';
